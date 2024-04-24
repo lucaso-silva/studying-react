54447300ef48bdaea6e11b4dbbed58d4a1d0e164
Lucas Oliveira <luc.osilva@gmail.com>
2024-04-23 20:56:32 -0700

    first steps react

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Header from './components/Header/Header.jsx'
+import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
+import { CORE_CONCEPTS } from './data.js'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image} />
+
+            {/* <CoreConcept 
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            /> */}
+
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CoreConcept/CoreConcept.jsx
@@ -0,0 +1,23 @@
+import './CoreConcept.css'
+
+// function CoreConcept(props) {
+//   return (
+//     <li>
+//       <img src={props.image} alt={props.title} />
+//       <h3>{props.title}</h3>
+//       <p>{props.description}</p>
+//     </li>
+//   )
+// }
+
+//WE CAN DESTRUCTURUING THE ABOVE FUNCTION USING THE SYNTAX BELOW:
+
+export default function CoreConcept({title, description, image}) {
+    return (
+      <li>
+        <img src={image} alt={title} />
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </li>
+    )
+  }
