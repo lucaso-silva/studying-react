ed49f19de2ac098dea7d1cc798ff49cb9b8b695c
Lucas Oliveira <luc.osilva@gmail.com>
2024-04-24 20:31:21 -0700

    working with useState()

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import Header from './components/Header/Header.jsx'
+import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
-import { CORE_CONCEPTS } from './data.js'
+import TabButton from './components/TabButton/TabButton.jsx';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
+
+import { useState } from 'react';
 
 function App() {
+  const [ selectedMessage, setSelectedMessage ] = useState("components");
+
+  function handleSelect(selectedButton) {
+    setSelectedMessage(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -25,6 +34,30 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id='examples'>
+            <h2>Examples</h2>
+
+            <menu>
+              <TabButton onSelect={()=>handleSelect("components")}>Components</TabButton>
+              <TabButton onSelect={()=>handleSelect("jsx")}>JSX</TabButton>
+              <TabButton onSelect={()=>handleSelect("props")}>Props</TabButton>
+              <TabButton onSelect={()=>handleSelect("state")}>State</TabButton>
+            </menu>
+
+            <div id="tab-content">
+              <h3>
+                {EXAMPLES[selectedMessage].title}
+              </h3>
+              <p>
+                {EXAMPLES[selectedMessage].description}
+              </p>
+              <pre>
+                <code>
+                  {EXAMPLES[selectedMessage].code}
+                </code>
+              </pre>
+            </div>
         </section>
       </main>
     </div>
